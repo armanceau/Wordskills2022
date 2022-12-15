f96e9280a63e3fa50b77bb31ddb581189130703b
armanceau2 <102350268+armanceau2@users.noreply.github.com>
2022-12-15 10:58:12 +0100

    update js

--- header.php
+++ header.php
@@ -6,4 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wordskills 2022</title>
     <link href="bootstrap-5.2.3-dist\css\bootstrap.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <script src="script.js"></script>
 </head>
--- project.php
+++ project.php
@@ -1,24 +1,50 @@
 
 <?php
     require 'header.php';
+    require 'sqlconnect.php';
 ?>
 
 <body>
     
+    <div>
+        <ul>
+            <li><a href="#" id="tous">Tous</a></li>
+            <li><a href="#" id="tous" >Mobile</a></li>
+            <div id ="mobile"></div>
+            
 
-    <?php
-        require 'sqlconnect.php';
+            <li><a href="#" id="web" onclick="web()">Web</a></li>
 
-        $sql = 'SELECT * FROM projet';
-        $table = $connection->query($sql);
-        while ($ligne = $table->fetch()) {
-                $title = $ligne['projet_title'];
-                $annee = $ligne['projet_annee'];
-                echo $ligne["projet_title"]."<br/>";
-                echo $ligne["projet_type"]."<br/>";
-                echo $ligne["projet_annee"]."<br/>";
-        
-        }
-    ?>
+
+
+            <li>Interacton</li>
+        </ul>
+    </div>
+    <form method="POSTE" action="getType.php">
+        <?php
+            
+
+            
+        ?>
+    </form>
+
+    <script>
+            function web(){
+                var mobile = document.getElementById('mobile');
+                mobile.innerHTML = "<?= 
+                        $sql = 'SELECT * FROM projet where projet_type=\'Web\'';
+                        $table = $connection->query($sql);
+                        while ($ligne = $table->fetch()) {
+                                $title = $ligne['projet_title'];
+                                $annee = $ligne['projet_annee'];
+                                echo $ligne["projet_title"]."<br/>";
+                                echo $ligne["projet_type"]."<br/>";
+                                echo $ligne["projet_annee"]."<br/>";
+                        
+                        }
+                    ?>";
+            }
+            
+            </script>
 </body>
 </html>
