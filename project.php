dd8d2c8a80c77cb23478a034f35487c933b93284
armanceau2 <102350268+armanceau2@users.noreply.github.com>
2022-12-15 13:35:46 +0100

    maj fonctionne

--- project.php
+++ project.php
@@ -1,50 +1,47 @@
 
 <?php
     require 'header.php';
-    require 'sqlconnect.php';
 ?>
 
 <body>
     
-    <div>
-        <ul>
-            <li><a href="#" id="tous">Tous</a></li>
-            <li><a href="#" id="tous" >Mobile</a></li>
-            <div id ="mobile"></div>
-            
 
-            <li><a href="#" id="web" onclick="web()">Web</a></li>
-
-
-
-            <li>Interacton</li>
-        </ul>
+    <div class="container">
+        <div style="text-align: center">
+            <ul>
+                <li><a href="#">Tous</a></li>
+                <li><a href="#">Mobile</a></li>
+                <li><a href="#">Web</a></li>
+                <li><a href="#">Interaction</a></li>
+            </ul>
+        </div>
     </div>
-    <form method="POSTE" action="getType.php">
+    <br>
+    <div class="container">
         <?php
-            
+            require 'sqlconnect.php';
 
+            $sql = 'SELECT * 
+                FROM projet, categorie 
+                WHERE projet.id_categorie=categorie.id_categorie
+                AND projet.id_categorie=1;
             
-        ?>
-    </form>
-
-    <script>
-            function web(){
-                var mobile = document.getElementById('mobile');
-                mobile.innerHTML = "<?= 
-                        $sql = 'SELECT * FROM projet where projet_type=\'Web\'';
-                        $table = $connection->query($sql);
-                        while ($ligne = $table->fetch()) {
-                                $title = $ligne['projet_title'];
-                                $annee = $ligne['projet_annee'];
-                                echo $ligne["projet_title"]."<br/>";
-                                echo $ligne["projet_type"]."<br/>";
-                                echo $ligne["projet_annee"]."<br/>";
-                        
-                        }
-                    ?>";
+            ';
+            $table = $connection->query($sql);
+            while ($ligne = $table->fetch()) {
+                    $title = $ligne['projet_title'];
+                    $annee = $ligne['projet_annee'];
+                    echo "<div class=\"col-3\">";
+                    echo $ligne["projet_title"]."<br/>";
+                    echo $ligne["projet_type"]."<br/>";
+                    echo $ligne["projet_annee"]."<br/>";
+                    echo '<img src="'.$ligne["projet_photo"].'" />';
+                    echo "</div>";
+                    echo "</br>";
+            
             }
-            
-            </script>
+        ?>
+    </div>
+    
 </body>
 </html>
