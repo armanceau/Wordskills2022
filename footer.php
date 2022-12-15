173d46fe9d8d06dcbf9dc6aaf6828d9b0936cf13
armanceau <100920811+armanceau@users.noreply.github.com>
2022-12-15 23:38:21 +0100

    nav footer + css bouton

--- footer.php
+++ footer.php
@@ -9,16 +9,61 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <footer>
-    <div class="container">
-        <div class="row">
-            <div class="6">
-                <p>2018 U|43 - Free Templates</p>
-            </div>
-            <div class="6">
-                <button type="button" class="rounded-pill bouton_lire">CONTACT</button>
+        <div class="row" style="padding-top: 10px;">
+            <div class="col-6">
+                <div class="row">
+                    <div class="col-5" style="margin-right:-190px;margin-left:140px;">
+                        <p>2018 UI43 - Free Templates</p>
+                    </div>
+                    <div class="col-2">
+                        <p>|</p>
+                    </div>
+                    <div class="col-5" style="margin-left:-140px;">
+                        <p>
+                            <a href="https://www.behance.net/">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21" viewBox="0 0 24 21">
+                                    <text id="behance" transform="translate(0 18)" fill="#8d8d8e" font-size="21" font-family="FontAwesome"><tspan x="0" y="0"></tspan></text>
+                                </svg>
+                            </a>
+
+                            <a href="https://dribbble.com/">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="21" viewBox="0 0 18 21">
+                                    <text id="dribbble" transform="translate(0 18)" fill="#8d8d8e" font-size="21" font-family="FontAwesome"><tspan x="0" y="0"></tspan></text>
+                                </svg>
+                            </a>
+
+                            <a href="https://www.linkedin.com/in/arthur-manceau-474671230/">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="21" viewBox="0 0 18 21">
+                                    <text id="in" transform="translate(0 18)" fill="#8d8d8e" font-size="21" font-family="FontAwesome"><tspan x="0" y="0"></tspan></text>
+                                </svg>
+
+                            </a>
+
+                            <a href="https://fr-fr.facebook.com/">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="21" viewBox="0 0 12 21">
+                                    <text id="facebook" transform="translate(0 18)" fill="#8d8d8e" font-size="21" font-family="FontAwesome"><tspan x="0" y="0"></tspan></text>
+                                </svg>
+                            </a>
+
+                            <a href="https://www.instagram.com/">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="21" viewBox="0 0 18 21">
+                                    <text id="instagram" transform="translate(0 18)" fill="#8d8d8e" font-size="21" font-family="FontAwesome"><tspan x="0" y="0"></tspan></text>
+                                </svg>
+                            </a>
+
+                            <a href="https://twitter.com/">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21">
+                                    <text id="twitter" transform="translate(0 18)" fill="#8d8d8e" font-size="21" font-family="FontAwesome"><tspan x="0" y="0"></tspan></text>
+                                </svg>
+                            </a>    
+                        </p>
+                    </div>
+                </div>      
+            </div>     
+            <div class="col-6">
+                <button type="button" class="rounded-pill bouton_contact_footer">CONTACT</button>
             </div>
         </div>
-    </div>
     
 </footer>
 </html>
