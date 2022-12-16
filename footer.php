7c2b36a77ff4e3274a00fcd90b83997919382c7a
armanceau2 <102350268+armanceau2@users.noreply.github.com>
2022-12-16 09:32:46 +0100

    update footer

--- footer.php
+++ footer.php
@@ -7,18 +7,19 @@
     <link href="bootstrap-5.2.3-dist\css\bootstrap.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="script.js" async></script>
 </head>
 <footer>
         <div class="row" style="padding-top: 10px;">
             <div class="col-6">
                 <div class="row">
-                    <div class="col-5" style="margin-right:-190px;margin-left:140px;">
+                    <div class="col-5" style="margin-right:-40px;margin-left:20px;">
                         <p>2018 UI43 - Free Templates</p>
                     </div>
                     <div class="col-2">
                         <p>|</p>
                     </div>
-                    <div class="col-5" style="margin-left:-140px;">
+                    <div class="col-5" style="margin-left:-90px;">
                         <p>
                             <a href="https://www.behance.net/">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21" viewBox="0 0 24 21">
@@ -61,7 +62,17 @@
                 </div>      
             </div>     
             <div class="col-6">
-                <button type="button" class="rounded-pill bouton_contact_footer">CONTACT</button>
+                <div class="row">
+                    <div class="col-7">
+
+                    </div>
+
+                    <div class="col-5">
+                        <button type="button" class="rounded-pill bouton_contact_footer">CONTACT</button>
+                    </div>
+                    
+                </div>
+                
             </div>
         </div>
     
